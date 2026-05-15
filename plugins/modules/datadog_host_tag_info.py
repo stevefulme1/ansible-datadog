27b214e9a85125c9a6e6b8829159d7d7399b117c
sfulmer <sfulmer@redhat.com>
2026-05-15 14:47:45 -0400

    Expand to 50 modules for full platform coverage

---
+++ plugins/modules/datadog_host_tag_info.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""datadog_host_tag_info module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: datadog_host_tag_info
+short_description: Retrieve host tag information
+description:
+    - Retrieve details about host tags.
+    - Read-only module.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    host:
+        description: API host address.
+        type: str
+        required: true
+    host_name:
+        description: ID of a specific resource.
+        type: str
+    name:
+        description: Filter by name.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: List all
+  stevefulme1.datadog.datadog_host_tag_info:
+    host: api.example.com
+  register: result
+
+- name: Get specific
+  stevefulme1.datadog.datadog_host_tag_info:
+    host: api.example.com
+    host_name: "example-id"
+  register: result
+"""
+
+RETURN = r"""
+host_tags:
+    description: List of resource details.
+    returned: always
+    type: list
+    elements: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from ansible_collections.stevefulme1.datadog.plugins.module_utils.api_client import ApiClient
+    HAS_CLIENT = True
+except ImportError:
+    HAS_CLIENT = False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            host_name=dict(type="str"),
+            name=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+    )
+
+    if not HAS_CLIENT:
+        module.fail_json(msg="Required Python libraries not found.")
+
+    client = ApiClient(module)
+    resource_id = module.params.get("host_name")
+
+    if resource_id:
+        result = client.get("host_tag", resource_id)
+        resources = [result] if result else []
+    else:
+        resources = client.list("host_tag", module.params)
+
+    module.exit_json(changed=False, host_tags=resources)
+
+
+if __name__ == "__main__":
+    main()
